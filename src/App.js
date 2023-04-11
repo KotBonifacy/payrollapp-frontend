a13c0d328e19f9621c3c044a6865642fe5eaea6d
KotBonifacy <124368590+KotBonifacy@users.noreply.github.com>
2023-04-12 00:33:54 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <input type="text" placeholder="Login" />
+    <input type="text" placeholder="Password" />
+    <button >Log in</button>    
+    </div>
+  );
+}
+
+export default App;
